c68ac280a3506158bdb00d259aa7a3c347f0b7d8
Jacfger <ncfung@ust.hk>
2021-08-11 16:34:32 +0800

    yadm upgrades lol

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,9 +1,10 @@
 set -g theme_color_scheme gruvbox
 set PATH /usr/lib $PATH
+set PATH ~/.cargo/bin $PATH
 
+# abbr smerge "flatpak run com.sublimemerge.App ./"
 
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
 eval /home/jacfger/anaconda3/bin/conda "shell.fish" "hook" $argv | source
 # <<< conda initialize <<<
-
